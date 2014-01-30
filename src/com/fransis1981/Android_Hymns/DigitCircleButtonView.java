64cb071091888f24ae1f8e627f7882f878c1112a
Francesco Vitullo <francesco.vitullo@gmail.com>
2014-01-31 00:28:04 +0100

    Some adjustments to the DigitCircleButton View.

--- src/com/fransis1981/Android_Hymns/DigitCircleButtonView.java
+++ src/com/fransis1981/Android_Hymns/DigitCircleButtonView.java
@@ -3,6 +3,7 @@ package com.fransis1981.Android_Hymns;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.widget.Button;
@@ -15,6 +16,7 @@ import android.widget.Button;
 public class DigitCircleButtonView extends Button {
     private Paint buttonPaint;
     private Paint textPaint;
+    private Rect txtBounds = new Rect();         //Helper object for centering text; preallocated for efficiency.
 
     int radius;
     // Color used to paint this button
@@ -30,21 +32,18 @@ public class DigitCircleButtonView extends Button {
                 buttonPaint.setStyle(Paint.Style.STROKE);
                 buttonPaint.setStrokeWidth(2);      //TODO: externalize as dimension the stroke width
 
-                radius = Math.min(getMeasuredHeight(), getMeasuredWidth())/2;
-
                 textPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
                 textPaint.setColor(HymnsApplication.myResources.getColor(android.R.color.darker_gray));
                 textPaint.setStrokeWidth(2);
                 textPaint.setStyle(Paint.Style.STROKE);
                 textPaint.setTextAlign(Paint.Align.CENTER);
-                textPaint.setTextSize(18);          //TODO: externalize as dimension the text size
 
                 lineColor = color;
             } catch (Exception e) {
                 Log.e(MyConstants.LogTag_STR, e.getMessage());
             }
         }
-     }
+    }
 
     public DigitCircleButtonView(Context context, int color) {
         super(context);
@@ -69,11 +68,18 @@ public class DigitCircleButtonView extends Button {
     @Override
     protected void onDraw(Canvas canvas) {
 
-        if (isInEditMode()) { super.onDraw(canvas); }
-        else {
-           canvas.drawARGB(0, 0, 0, 0);
-           canvas.drawCircle(radius, radius, radius - 3, buttonPaint);
-           if (getText().length() > 0) canvas.drawText(getText().toString(), radius, radius, textPaint);    //TODO: externalize half the text
-       }
+        if (isInEditMode()) {
+            super.onDraw(canvas);
+        } else {
+            radius = Math.min(getMeasuredHeight(), getMeasuredWidth()) / 2;
+            canvas.drawARGB(0, 0, 0, 0);
+            canvas.drawCircle(radius, radius, radius - 3, buttonPaint);
+            textPaint.setTextSize(radius - 4);          //TODO: externalize as dimension the text size
+            if (getText().length() > 0) {
+                textPaint.getTextBounds(getText().toString(), 0, 1, txtBounds);
+                Log.i(MyConstants.LogTag_STR, "centerY(): " + txtBounds.centerY());
+                canvas.drawText(getText().toString(), radius, radius - txtBounds.centerY(), textPaint);
+            }
+        }
     }
 }
