12ae1237f3e06fd7da9520d49c70bf389e8fa119
Francesco Vitullo <francesco.vitullo@gmail.com>
2014-02-06 15:46:54 +0100

    Removed from index directories with auto-generated content.

--- gen/com/fransis1981/Android_Hymns/R.java
+++
@@ -1,7 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package com.fransis1981.Android_Hymns;
-
-/* This stub is only used by the IDE. It is NOT the R class actually packed into the APK */
-public final class R {
-}
