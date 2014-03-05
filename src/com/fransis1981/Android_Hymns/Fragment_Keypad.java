740cfef18c2c22d807bee662c54f41980f9e9fbf
Francesco Vitullo <francesco.vitullo@gmail.com>
2014-03-05 11:14:37 +0100

    WIP - Implementing ViewPager on the main screen.

---
+++ src/com/fransis1981/Android_Hymns/Fragment_Keypad.java
@@ -0,0 +1,9 @@
+package com.fransis1981.Android_Hymns;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Fransis on 05/03/14 11.05.
+ */
+public class Fragment_Keypad extends Fragment {
+}
